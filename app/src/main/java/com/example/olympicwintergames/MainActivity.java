ca50123072f222373ab669faf4f39cdd3b4c808f
Santiago Donadio <santiagodonadio@gmail.com>
2026-02-18 18:00:15 -0500

    Start screen goes to second page

--- app/src/main/java/com/example/olympicwintergames/MainActivity.java
+++ app/src/main/java/com/example/olympicwintergames/MainActivity.java
@@ -8,6 +8,9 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import android.content.Intent;
+
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -21,4 +24,10 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
     }
+
+    public void goToHome(android.view.View view) {
+        Intent intent = new Intent(this, MainActivity2.class);
+        startActivity(intent);
+    }
+
 }
