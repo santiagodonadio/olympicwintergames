11b1da52e597b9b9648d794b9679a467ee0fe245
Santiago Donadio <santiagodonadio@gmail.com>
2026-02-19 11:01:08 -0500

    Added snowboard image with logic

--- app/src/main/java/com/example/olympicwintergames/MainActivity2.java
+++ app/src/main/java/com/example/olympicwintergames/MainActivity2.java
@@ -1,6 +1,8 @@
 package com.example.olympicwintergames;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,13 @@ public class MainActivity2 extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+
+    public void goToSnowboard(View view) {
+
+        Intent intent = new Intent(this, MainActivity3.class);
+        startActivity(intent);
+
     }
 }
